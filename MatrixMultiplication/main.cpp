6ea2ebb73a01e77ef820da62f023e881f808038e
Adam Emieljaniuk <beginner1986@gmail.com>
2019-12-05 20:25:06 +0100

    Minor changes and screen with results added.

--- MatrixMultiplication/main.cpp
+++ MatrixMultiplication/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <iomanip>
 #include <string>
 #include <chrono>
 #include <vector>
@@ -8,12 +9,13 @@
 double multiplyI(const Matrix& A, const Matrix& B, Matrix& result, int threadsNumber);
 double multiplyJ(const Matrix& A, const Matrix& B, Matrix& result, int threadsNumber);
 double multiplyK(const Matrix& A, const Matrix& B, Matrix& result, int threadsNumber);
-
+void printTable(double table[5][6]);
 
 int main()
 {
 	// polish language support
 	setlocale(LC_ALL, "polish");
+	std::cout << std::setprecision(5) << std::fixed;
 
 	// read the matrices to multiply
 	Matrix* A, * B;
@@ -24,20 +26,26 @@ int main()
 	double timesJ[5][6];
 	double timesK[5][6];
 
+	// top - left field
+	timesI[0][0] = 0;
+	timesJ[0][0] = 0;
+	timesK[0][0] = 0;
+	
+	// first column (sizes)
 	for (size_t i=0; i<sizes.size(); i++)
 	{
-		timesI[i][0] = sizes[i];
-		timesJ[i][0] = sizes[i];
-		timesK[i][0] = sizes[i];
+		timesI[i + 1][0] = sizes[i];
+		timesJ[i + 1][0] = sizes[i];
+		timesK[i + 1][0] = sizes[i];
 	}
 
 	// test cases
-	for (int numberOfThreads = 1; numberOfThreads <= 16; numberOfThreads *= 2)
+	for (int numberOfThreads = 1, column = 1; numberOfThreads <= 16; numberOfThreads *= 2, column++)
 	{
-
 		// result arrays columns initialization
-		int column = 0;
 		timesI[0][column] = numberOfThreads;
+		timesJ[0][column] = numberOfThreads;
+		timesK[0][column] = numberOfThreads;
 
 		for (size_t row=0; row<sizes.size(); row++)
 		{
@@ -47,6 +55,7 @@ int main()
 			std::string matrixBFileName = "b" + std::to_string(size);
 			std::string matrixCFileName = "c" + std::to_string(size);
 
+			omp_set_num_threads(2);
 			#pragma omp parallel
 			{
 				#pragma omp sections
@@ -65,24 +74,45 @@ int main()
 			// prepare the result matrix
 			Matrix *C = new Matrix(A->getN(), B->getM());
 
-			// multiply the matrices
-			timesI[row][column] = multiplyI(*A, *B, *C, numberOfThreads);
+			// multiply the matrices, save result to file and count time
+			timesI[row + 1][column] = multiplyI(*A, *B, *C, numberOfThreads);
 			C->writeToFile(matrixCFileName + "i");
-			timesJ[row][column] = multiplyJ(*A, *B, *C, numberOfThreads);
+			timesJ[row + 1][column] = multiplyJ(*A, *B, *C, numberOfThreads);
 			C->writeToFile(matrixCFileName + "j");
-			timesK[row][column] = multiplyK(*A, *B, *C, numberOfThreads);
+			timesK[row + 1][column] = multiplyK(*A, *B, *C, numberOfThreads);
 			C->writeToFile(matrixCFileName + "k");
-
-			// save result to file
 
 			// free the memory
 			delete A, B, C;
 		}
 	}
 
+	// print the times
+	std::cout << "i - loop:" << std::endl;
+	printTable(timesI);
+	std::cout << "j - loop:" << std::endl;
+	printTable(timesJ);
+	std::cout << "k - loop:" << std::endl;
+	printTable(timesK);
+
 	// hold the screen
 	std::cin.get();
+	int temp;
+	std::cin >> temp;
 	return 0;
+}
+
+void printTable(double table[5][6])
+{
+	for (int i = 0; i < 5; i++)
+	{
+		for (int j = 0; j < 6; j++)
+		{
+			std::cout << (i==0 || j==0 ? std::setprecision(0) : std::setprecision(5)) << std::fixed << table[i][j] << "\t" << (table[i][j] > 10.0f ? "" : "\t");
+		}
+		std::cout << std::endl;
+	}
+	std::cout << std::endl;
 }
 
 double multiplyI(const Matrix& A, const Matrix& B, Matrix &result, int threadsNumber)
